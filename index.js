811607a18bb5b03afc017a1d242b14d5b880b616
RAJU3519 <143126137+RAJU3519@users.noreply.github.com>
2024-07-20 12:40:12 +0530

    git hub add data

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/login',(req,res)=>{
     res.send('<h1>Welcome to login page</h1>')
 })
 
+app.get('/youtube',(req,res)=>{
+    res.send('<h1>Welcome to youtube page</h1>')
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
